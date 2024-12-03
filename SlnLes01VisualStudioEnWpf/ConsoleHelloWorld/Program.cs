1763a92d14cd289463cbf1871d6042239cf7da5a
anaselbouj <anas.elboujdaini@student.odisee.be>
2024-12-03 10:58:49 +0100

    helloworld 1st push

---
+++ SlnLes01VisualStudioEnWpf/ConsoleHelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHalloWereld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Wereld!");
+            Console.Write("Hoe heet je: ");
+            string naam = Console.ReadLine();
+            Console.WriteLine("Aangename kennismaking " + naam);
+            Console.WriteLine("druk een toets om verder te gaan... ");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
